e654c13cd685d26e63a23e1233b7c33cebaef6d5
Safari Ousman <safariousman02@gmail.com>
2026-09-23 23:24:34 +0100

    maling the text to display in a new div

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
 const inputBx = document.querySelector(".input-box");
+const fontList = document.querySelector("#fontList");
 
-console.log(inputBx.value);
+const styles = [
+  "editorial-serif",
+  "silk-monospace",
+  "whispering-script",
+  "small-caps-spaced",
+  "circled-minimal",
+  "fraktur-archival",
+  "double-struck",
+];
+
+inputBx.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+
+    const text = inputBx.value;
+
+    if (!text) return;
+
+    createFontCard(text);
+    console.log("Text: ", inputBx.value);
+  }
+});
+
+function createFontCard(text) {
+  console.log("drive: ", text);
+
+  const tile = document.createElement("div");
+
+  tile.className = "font-card";
+
+  tile.innerHTML = `
+    
+    <div class="font-card">
+        <div class="font-card-header">
+            <div class="font-title">
+                <span class="dot"></span> Editorial Serif
+            </div>
+            <div class="font-meta">Claude: Bookish & Book</div>
+            <div class="font-actions">
+                <button>⬇</button>
+                <button>♡</button>
+            </div>
+        </div>
+        <div class="font-preview editorial-serif">
+            ${text}
+        </div>
+        <div class="font-footer">
+            <span>Uncode Elegant Italic Serif</span>
+            <button>📋 Copy</button>
+        </div>
+    </div> 
+ `;
+
+  fontList.appendChild(tile);
+}
